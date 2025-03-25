468b17b2bef91cf1d7b2d6bb5682344c914d1e1c
stopons <199826919+stopons@users.noreply.github.com>
2025-03-25 15:20:37 +0100

    Code (4_findsegments.py)
    - Identified issue with 'd' not appearing in output
    
    Need to make sure why "d" is not showing off.
    There's a clear problem in my algo regarding the last 4 chars.

--- week1/4_findsegments.py
+++ week1/4_findsegments.py
@@ -1,6 +1,40 @@
 def find_segments(data):
-    # TODO
-    pass
+
+    segments = []
+    
+    data_list = list(data)
+    counter = 1
+    print(data_list)
+
+    # Create a for loop to iterate through "data_list", incrementing "counter" each time the
+    # character is the same as the previous one (or None in case of the first character).
+    # When the character is different, we'll append everything accumulated so far and reset
+    # the count for the new character.
+
+    for i in range(len(data_list)):
+        # Initialize the list at the beginning of the for loop to be able to use ".append",
+        # and then convert it into a tuple.
+        segment_item = [] 
+
+        if i == 0:
+            continue
+        elif i == len(data_list):
+            segment_item.append(counter)
+            segment_item.append(data_list[i-1])
+            segment_item = tuple(segment_item)
+            segments.append(segment_item)
+            break
+        elif data_list[i] == data_list[i-1]:
+            counter += 1
+        else:
+            segment_item.append(counter)
+            segment_item.append(data_list[i-1])
+            segment_item = tuple(segment_item)
+            segments.append(segment_item)
+            counter = 1 #Reset the counter at the end
+
+
+    return segments
 
 
 
@@ -10,11 +44,11 @@ if __name__ == "__main__":
     print(find_segments("aaabbccdddd"))
     # [(3, 'a'), (2, 'b'), (2, 'c'), (4, 'd')]
 
-    print(find_segments("aaaaaaaaaaaaaaaaaaaa"))
+    #print(find_segments("aaaaaaaaaaaaaaaaaaaa"))
     # [(20, 'a')]
 
-    print(find_segments("abcabc"))
+    #print(find_segments("abcabc"))
     # [(1, 'a'), (1, 'b'), (1, 'c'), (1, 'a'), (1, 'b'), (1, 'c')]
 
-    print(find_segments("kissa"))
+    #print(find_segments("kissa"))
     # [(1, 'k'), (1, 'i'), (2, 's'), (1, 'a')]
